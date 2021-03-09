b2c8e35c800784b68c613dd624eb7c92c13b9bfa
Dennis Wong <denniswong0106@gmail.com>
2021-03-09 08:28:31 +0000

    added timer1.js, works with sound, and tested for negative number input (outputs error message) and non-number inputs (outputs error message)

---
+++ timer1.js
@@ -0,0 +1,19 @@
+const timer = () => {
+  timesArr = process.argv.slice(2).sort((a, b) => a - b);
+  for (const e of timesArr) {
+    if (Number(e) < 0) {
+      console.log('Please put Valid Number!')
+      break;
+    }
+    if (typeof Number(e) === 'number') {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+        console.log("WAKE UP!!!!! WAKE UP!!!!! WAKE UP!!!!")
+      }, Number(e) * 1000)
+    } else  {
+      console.log("return a number!")
+    }
+  }
+}
+
+timer();
